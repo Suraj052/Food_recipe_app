df4754bc955606fef8af4c2757c9e3896da1716e
Suraj Mahapatra <surajjiku@gmail.com>
2022-06-15 20:02:41 +0530

    done

---
+++ Source Code/Recipe/app/src/main/java/com/example/recipeapp/entities/Category.kt
@@ -0,0 +1,20 @@
+package com.example.recipeapp.entities
+
+import androidx.room.*
+import com.example.recipeapp.entities.converter.CategoryListConverter
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "Category")
+data class Category(
+    @PrimaryKey(autoGenerate = true)
+    var id:Int,
+
+    @ColumnInfo(name = "categorieitems")
+    @Expose
+    @SerializedName("categories")
+    @TypeConverters(CategoryListConverter::class)
+    var categorieitems: List<CategoryItems>? = null
+
+)
